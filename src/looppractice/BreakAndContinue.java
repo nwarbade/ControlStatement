bbe20270ca95f0ac2f3b60f061aa9d52554c8e6f
nwarbade <nwarbade@gmail.com>
2023-07-21 11:49:41 +0530

    added new

---
+++ src/looppractice/BreakAndContinue.java
@@ -0,0 +1,23 @@
+package looppractice;
+
+public class BreakAndContinue {
+	
+		public static void main(String[] args) {
+		
+		int i = 10;
+		while(i<=50)
+		{
+			System.out.println("Hello");
+			
+			if(i==20)
+			{
+				break;
+			}
+			i++;
+		}
+		
+		
+		
+	}
+
+}
---
+++ src/looppractice/ReverseOfNumber.java
@@ -0,0 +1,36 @@
+package looppractice;
+
+public class ReverseOfNumber {
+	
+public static void main(String[] args) {
+		
+		int number = 121;
+		int remainder; 
+		String reverse = "";
+		
+		String originalnum = String.valueOf(number);
+	
+		while(number!=0)
+		{
+			remainder = number %10;
+			System.out.print(remainder);
+			
+			reverse = reverse  + remainder;
+			
+			
+			number = number /10;
+						
+		}
+		
+		if(originalnum.equals(reverse))
+		{
+			System.out.println("Number is palindrome");
+		}
+		else
+		{
+			System.out.println("Not palindrome");
+		}
+		
+	}
+
+}
